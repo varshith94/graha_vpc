3ee99a1edcb01db055c0f59c014c1cab2efe29ca
varshith94 <varshithvenkatsai5822@gmail.com>
2023-12-07 10:55:56 +0530

    updatedfile

--- subnet.tf
+++ subnet.tf
@@ -1,6 +1,6 @@
 resource "aws_subnet" "subnets" {
   vpc_id = aws_vpc.test.id
-  count = 4
+  count = length(var.azs)
   cidr_block = element(var.public_cidr_block,count.index)
   map_public_ip_on_launch = true
   availability_zone = element(var.azs,count.index)
